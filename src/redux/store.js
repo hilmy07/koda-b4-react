719436425e63a658526e30a195c5fb1b6129f7a6
Muhammad Hilmy Haidar A <85333182+hilmy07@users.noreply.github.com>
2026-02-04 17:05:57 +0700

    fix: Update Redux store to use combined reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // default: localStorage
 import { combineReducers } from "redux";
 import cartReducer from "./reducers/chart";
+import reducers from "./reducers";
 
 // Konfigurasi persist
 const persistConfig = {
@@ -13,11 +14,11 @@ const persistConfig = {
 };
 
 // Gabungkan semua reducer
-const rootReducer = combineReducers({
-  cart: cartReducer,
-});
+// const rootReducer = combineReducers({
+//  cart: cartReducer,
+// });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 // Buat store
 export const store = configureStore({
